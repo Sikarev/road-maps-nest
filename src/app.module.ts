da88de5f84cde2aab226bb5f664c26f1fe4dc718
Sikarev <sikarev.roman@yandex.ru>
2023-08-06 13:39:22 +0300

    Added Dockerfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     HttpModule.register({
       timeout: 100000,
       baseURL: 'https://its62.ru/static-cache/',
